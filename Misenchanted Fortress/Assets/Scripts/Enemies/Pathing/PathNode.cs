449c8c2d073aadfada0a12d23d430502705a32fe
Jack Hoffman <hoffmanjack713@gmail.com>
2020-10-05 13:05:45 -0400

    Created Simple Pathing System for Enemies
    Created 2 Scripts
    
    FollowPath - Follows an array of PathNodes and stops at the end
    
    PathNode - Draws a square around a PathNode while gizmos are enabled
    
    Created 1 Prefab
    
    PathNode - Can be placed to indicate inflection points in the pathing of enemies
    
    Created 1 Scene for enemy testing

---
+++ Misenchanted Fortress/Assets/Scripts/Enemies/Pathing/PathNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Draw a red gizmo box
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, new Vector3(0.5f, 0.5f, 0.5f));
+    }
+}
