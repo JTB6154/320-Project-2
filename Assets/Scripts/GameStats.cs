4e93e4f93c04dc43005c1dcf55dab071130c45b3
JTB6154 <jtb6154@rit.edu>
2020-10-07 14:16:33 -0400

    created Gamestats
    Created the game stats singleton, and added the tower type enum at the top.

---
+++ Assets/Scripts/GameStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TowerType { 
+	Archer,
+	Wizard,
+	Theurgist
+}
+
+public class GameStats : Singleton<GameStats>
+{
+	Dictionary<TowerType, int> Costs = new Dictionary<TowerType, int>();
+
+	public int getCost(TowerType type)
+	{
+		return Costs[type];
+	}
+
+
+}
