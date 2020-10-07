31ee20292070d82bb4c245daef0bd30de065e187
JTB6154 <jtb6154@rit.edu>
2020-10-07 14:24:54 -0400

    Added new method stubs to gamestats
    Added new stubs to the gamestats

--- Assets/Scripts/GameStats.cs
+++ Assets/Scripts/GameStats.cs
@@ -11,11 +11,43 @@ public enum TowerType {
 public class GameStats : Singleton<GameStats>
 {
 	Dictionary<TowerType, int> Costs = new Dictionary<TowerType, int>();
+	int playerCash = 19;
+
+	public void initialize()
+	{
+
+	}
 
 	public int getCost(TowerType type)
 	{
 		return Costs[type];
 	}
 
+	/// <summary>
+	/// Manages the player gold for purchasing an item of that type
+	/// </summary>
+	/// <param name="type"></param>
+	public bool purchaseItemOfType(TowerType type)
+	{
+		if (Costs[type] > playerCash)
+		{
+			return false;
+		}
+		else
+		{
+			playerCash -= Costs[type];
+			return true;
+		}
+	}
+
+
+	/// <summary>
+	/// adds the specified amount of gold to the players inventory
+	/// </summary>
+	/// <param name="amount">the amount of gold to add to the players inventory</param>
+	public void addGold(int amount)
+	{
+		playerCash += amount;
+	}
 
 }
