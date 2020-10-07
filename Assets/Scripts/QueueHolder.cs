cb1e91248fe501027f34219ee88a8d0801fd18a4
Vansh Pahuja <vp2350@rit.edu>
2020-10-07 01:19:49 -0400

    Added code for targetting
    Added code for targetting in TowerScene, QueueHolder is a placeholder for game manager

---
+++ Assets/Scripts/QueueHolder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Replace with the game manager
+public class QueueHolder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Queue<GameObject> objectQueue;
+    void Start()
+    {
+        this.objectQueue= new Queue<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
