d6517b5aca280f708c6efe7afa338211a1746ff8
JTB6154 <jtb6154@rit.edu>
2020-10-07 14:35:16 -0400

    created initialization for game stats
    created the initialization function for gamestats
    Improved comments for some functions

--- Assets/Scripts/GameStats.cs
+++ Assets/Scripts/GameStats.cs
@@ -10,12 +10,24 @@ public enum TowerType {
 
 public class GameStats : Singleton<GameStats>
 {
-	Dictionary<TowerType, int> Costs = new Dictionary<TowerType, int>();
+	Dictionary<TowerType, int> Costs = new Dictionary<TowerType, int>();;
 	int playerCash = 19;
+	bool hasBeenInitialized = false;
 
 	public void Initialize()
 	{
+		//only initialize if we haven't been before
+		if (hasBeenInitialized) return;
+		//initialize any arrays or dictionaries in the Singleton
 
+		//initialize the costs dictionary
+		Costs[TowerType.Archer] = 10;
+		Costs[TowerType.Wizard] = 10;
+		Costs[TowerType.Theurgist] = 10;
+
+
+		//mark that the class has been initialized
+		hasBeenInitialized = true;
 	}
 
 	public int GetCost(TowerType type)
@@ -24,9 +36,10 @@ public class GameStats : Singleton<GameStats>
 	}
 
 	/// <summary>
-	/// Manages the player gold for purchasing an item of that type
+	/// Returns wether or not you can purchase the tower of given type and if you can removes the gold
 	/// </summary>
-	/// <param name="type"></param>
+	/// <param name="type">The type of tower you are attempting to purchase</param>
+	/// <returns>returns true if the tower was successfully purchased, and false if it was not</returns>
 	public bool PurchaseItemOfType(TowerType type)
 	{
 		if (Costs[type] > playerCash)
