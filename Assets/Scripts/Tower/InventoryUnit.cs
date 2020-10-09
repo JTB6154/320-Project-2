8421a3beb77aea645f19dfb719e226ad92d026b8
JTB6154 <jtb6154@rit.edu>
2020-10-09 15:12:25 -0400

    Change to UI based system
    Pivoted unit assignment from inventory to be a UI based system as opposed to the game object based system previously in operation.

--- Assets/Scripts/Tower/InventoryUnit.cs
+++ Assets/Scripts/Tower/InventoryUnit.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class InventoryUnit : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Fields
+    [SerializeField] TowerType type = TowerType.None;
+    public TowerType Type
+    {
+        get { return type; }
+    }
+    [Space]
+    int level = -1; //initialize to negative one so we can call level up to initialize
+    [SerializeField] float baseRange;
+    [SerializeField] float rangePerLevel;
+    [Space]
+    [SerializeField] int baseDamage;
+    [SerializeField] int damagePerLevel;
+    [Space]
+    [SerializeField] float baseAttackSpeed;
+    [SerializeField] float attackSpeedPerLevel;
+
+    float range, attackSpeed;
+    int damage;
+
+    #endregion
     void Start()
     {
+        //initializeto level one
+        levelUp();
+    }
+
+    public void Highlight()
+    { 
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void levelUp()
     {
-        
+        level++;
+        range = updateStatLevel(baseRange, rangePerLevel);
+        damage = updateStatLevel(baseDamage, damagePerLevel);
+        attackSpeed = updateStatLevel(baseAttackSpeed, attackSpeedPerLevel);
     }
+
+    int updateStatLevel(int baseStat, int incrementPerLevel)
+    {
+        return baseStat + incrementPerLevel * level;
+    }
+
+    float updateStatLevel(float baseStat, float incrementPerLevel)
+    {
+        return baseStat + incrementPerLevel * level;
+    }
+
+
+
 }
--- Assets/Scripts/UnitAssignment.cs
+++ Assets/Scripts/UnitAssignment.cs
@@ -8,6 +8,10 @@ public class UnitAssignment : MonoBehaviour
     #region Fields
     //list of active towers
     //inventory of units
+    List<InventoryUnit> inventory = new List<InventoryUnit>();
+    [SerializeField] List<GameObject> buttons = new List<GameObject>();
+    [SerializeField] Sprite ArcherSprite, WizardSprite, TheurgistSprite, NoneSprite;
+    Dictionary<TowerType, Sprite> towerToSprite = new Dictionary<TowerType, Sprite>();
     //reference to currently highlighted unit
     bool unitHighlighted;
 	#endregion
@@ -15,36 +19,29 @@ public class UnitAssignment : MonoBehaviour
 	void Start()
     {
         //initialize variables
+        GameStats.Instance.Initialize();
+        towerToSprite.Add(TowerType.Archer, ArcherSprite);
+        towerToSprite.Add(TowerType.Wizard, WizardSprite);
+        towerToSprite.Add(TowerType.Theurgist, TheurgistSprite);
+        towerToSprite.Add(TowerType.None, NoneSprite);
     }
 
     void Update()
     {
-
-        if (Input.GetMouseButtonDown(0))
+        for (int i = 0; i < buttons.Count; i++)
         {
-
-            if (unitHighlighted)
-            {
-                //if what you've clicked on is a new unit, highlight it
-
-                //if what you've clikced on is a tower, move the unit to a tower
-
-            }
-            else
-            {
-                //highlight the new unit
-                
-            }
-
-
+            buttons[i].SetActive( inventory[i].Type != TowerType.None);
+            buttons[i].GetComponent<SpriteRenderer>().sprite = towerToSprite[inventory[i].Type];
         }
 
-
-
-        //see if a unit is clicked on
-
-        //if the unit has been selected highlight it
-        //unhighlight the old unit
-        
     }
+
+    //we'll give the buttons this function and the proper index
+    void displayInventoryItem(int itemIndex)
+    { 
+        //get some data about the inventory items
+        //send it to a lable of some sort
+    }
+
+
 }
