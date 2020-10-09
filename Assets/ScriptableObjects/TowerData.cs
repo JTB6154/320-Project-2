d01c7884a35761884ddd8ec7873af0dc0674c9c7
theflyingpinata <42786585+theflyingpinata@users.noreply.github.com>
2020-10-09 14:48:41 -0400

    Refactored GameStats to use TowerData

---
+++ Assets/ScriptableObjects/TowerData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Tower", menuName = "New Tower")]
+public class TowerData : ScriptableObject
+{
+    // Name
+    public string TowerName;
+
+    // Combat Stats
+    public int Cost;
+    public float[] AttackSpeed = new float[3];
+    public float[] Damage = new float[3];
+    public float[] Range = new float[3];
+
+    // Image data and such
+    public Sprite ShopPortrait;
+}
