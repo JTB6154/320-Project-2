0926eea7c81be970f2e9b10bd8872bac8b561919
Jack Hoffman <hoffmanjack713@gmail.com>
2020-10-21 14:02:20 -0400

    Added simple sounds for attacking and death of enemies

---
+++ Assets/Scripts/Enemies/Behaviors/BasicRanged.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicRanged : Enemy
+{
+    public override void Start()
+    {
+        base.Start();
+        print(Application.dataPath + "/Audio/Enemy/BasicMelee/attack.wav");
+        attackNoise = Resources.Load<AudioClip>(Application.dataPath + "/Audio/Enemy/BasicMelee/attack.wav");
+        deathNoise = Resources.Load<AudioClip>(Application.dataPath + "/Audio/Enemy/BasicMelee/death.wav");
+    }
+
+    /// <summary>
+    /// Attacks the player health
+    /// </summary>
+    public override void Attack()
+    {
+        // Do math
+        base.Attack();
+    }
+
+    /// <summary>
+    /// Gives the player the value of the unit into their 
+    /// </summary>
+    public override void Die()
+    {
+        // Do math
+        base.Die();
+        print("did this one ma");
+    }
+}
