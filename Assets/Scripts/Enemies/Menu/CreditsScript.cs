948171754f6e9b842ea5d9c61499c031ba5743a8
Vansh Pahuja <vp2350@rit.edu>
2020-10-26 15:39:40 -0400

    Credits

---
+++ Assets/Scripts/Enemies/Menu/CreditsScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScript : MonoBehaviour
+{
+    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject uiPanel;
+
+    public void ActivateMenu()
+    {
+        uiPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        uiPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+
+}
